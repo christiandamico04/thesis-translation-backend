c5197b551c248f3bbe1b992300c9c0e559b2460d
Christian D’Amico <mashalem@Host-005.aitech.vision>
2025-06-12 17:07:04 +0200

    Register File and Translation in Django Admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import File, Translation
 
 # Register your models here.
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+
+@admin.register(Translation)
+class TranslationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'file', 'src_language', 'dst_language', 'status', 'created_at')
+    list_filter = ('status', 'src_language', 'dst_language')
