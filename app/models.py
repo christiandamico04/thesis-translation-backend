18a50be5c3dc6ab570ae1c87f02a39848d698995
Christian D’Amico <mashalem@Host-004.aitech.vision>
2025-06-16 15:28:59 +0200

    Implementing delete function

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files.storage import default_storage
 
 # Create your models here.
 
@@ -16,6 +17,22 @@ class File(models.Model):
     status = models.CharField(max_length=20, default='uploaded')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def delete(self, *args, **kwargs):
+        # Controlla se c'è un file associato prima di fare qualsiasi cosa
+        if self.file:
+            # Memorizza il nome del file prima di cancellare l'oggetto
+            file_name = self.file.name
+            
+            # Cancella il record dal DB
+            super().delete(*args, **kwargs)
+            
+            # Ora, usa l'API di storage per cancellare il file fisico
+            if default_storage.exists(file_name):
+                default_storage.delete(file_name)
+        else:
+            # Se non c'era nessun file, cancella solo il record
+            super().delete(*args, **kwargs)
 
 class Translation(models.Model):
     # PK id generata automaticamente
